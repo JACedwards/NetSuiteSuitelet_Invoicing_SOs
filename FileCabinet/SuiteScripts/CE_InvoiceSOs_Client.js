06a69721a3691126a4f0012b7b5d8a657db7241c
JACedwards <98490993+JACedwards@users.noreply.github.com>
2024-03-18 15:10:21 -0400

    first commit

---
+++ FileCabinet/SuiteScripts/CE_InvoiceSOs_Client.js
@@ -0,0 +1,191 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/currentRecord', 'N/record'],
+
+    /**
+    * @param {currentRecord} currentRecord
+    * @param {record} record
+    * @param {Object} scriptContext
+    * @param {Record} scriptContext.currentRecord - Current form record
+    * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+    */
+
+    function(currentRecord, record) {
+        
+        //filters sales orders by customer
+        // function filterCustomers (event) {
+        //     let url = window.location.href;
+        //     let urlBase = findBaseUrl(url);
+        //     let currentRec = currentRecord.get();
+        //     let customers = currentRec.getValue({
+        //         fieldId: 'custpage_multiselect'
+        //     });
+    
+        //     /** handles error if user clicks Customer Filter button, without 
+        //         selecting customers. */
+        //     if (customers[0] === ''){
+        //         debugger;
+        //         window.location.href = urlBase + '&customers_filtered=AAA';
+        //     }
+
+        //     else{
+        //         customers = customers.join(',');
+        //         window.onbeforeunload = null;
+        //         window.location.href = urlBase + '&customers_filtered=' + customers;
+        //     }
+        // }
+
+        // function filterStatus (event) {
+        //     let url = window.location.href;
+        //     let urlBase = findBaseUrl(url);
+        //     let currentRec = currentRecord.get();
+        //     let status = currentRec.getValue({
+        //         fieldId: 'custpage_selectstatus'
+        //     });
+        //     // debugger;
+        //     // console.log('status', status);
+        //     window.onbeforeunload = null;
+        //     window.location.href = urlBase + '&status=' + status;
+        // }
+
+        //combining both customer and status filters
+
+        function filterAll (event) {
+            debugger;
+            let url = window.location.href;
+            let urlBase = findBaseUrl(url);
+            let currentRec = currentRecord.get();
+            let customers = currentRec.getValue({
+                fieldId: 'custpage_multiselect'
+            });
+            customers = customers.join(',');
+            let customerURL = '&customers_filtered=' + customers;
+
+            let status = currentRec.getValue({
+                fieldId: 'custpage_selectstatus'
+            });
+            let statusURL = '&status=' + status
+    
+            window.onbeforeunload = null;
+            window.location.href = urlBase + customerURL + statusURL;
+        }
+
+        /** Error handling when user clicks Invoice All button without having checked any sales orders to invoice*/
+        
+        //Gets base URL from custom record to simplify functions in this file
+        function noSelections (event) {
+
+            const fetchSuiteletUrl = record.load({
+                type : 'customrecord_ce_inv_mr_results',
+                id: 1
+            });
+
+            let suiteletUrl = fetchSuiteletUrl.getValue({
+                fieldId : 'custrecord_suitlet_base_url'
+            });
+
+            let currentRec = currentRecord.get();
+            let hiddenCustIds = currentRec.getValue({
+                fieldId: 'custpage_hidden_cust_filter'
+            });
+            
+            if (hiddenCustIds === undefined){
+                window.location.href = suiteletUrl;
+            }
+                
+            else {
+                if (hiddenCustIds === ''){
+                    window.location.href = suiteletUrl;
+                }
+                //Allows any filtered customer id's to persist, when button clicked to return to start page, 
+                //    after Invoice All button clicked with no SO's checked
+                else {
+                    hiddenCustIds = hiddenCustIds.replaceAll('\u0005',',');           
+                    window.location.href = suiteletUrl + '&customers_filtered=' + hiddenCustIds
+                }
+
+            }
+        }
+    
+
+        //Clears customer multi-select (because built-in Reset button didn't work).
+        function clearCustomers (event) {
+            let url = window.location.href;
+            let urlBase = findBaseUrl(url);
+            console.log('urlBase', urlBase);
+            window.location.href = urlBase
+        }
+
+        // No longer needed due to simpler method of storing/accessing base URL
+        //      on custom record.
+        function findBaseUrl(url){
+
+            let l = 0;
+            let r = 7;
+            let equalSign = 0
+            let baseUrl = ''
+            let flag = true
+            while (flag) {
+        
+                if (url.slice(l,r) === 'deploy='){
+                    baseUrl = url.slice(l,r)
+                    equalSign = r
+                    flag = false
+                }
+                else{
+                    l+=1;
+                    r+=1;
+                }
+            }
+        
+            console.log('baseUrl if block = ' + baseUrl)
+            console.log('equalSign = ' + equalSign)
+        
+            flag = true
+            while (flag) {
+        
+        
+                if (url[equalSign] === '&'){
+                    url = url.slice(0, equalSign)
+                    flag = false
+                }
+                
+                else if (equalSign > url.length){
+                    url = url
+                    flag = false
+                }
+        
+                equalSign +=1
+            }
+            return url;
+        }
+        
+
+
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+
+    function pageInit(scriptContext) {
+        }
+
+    return {
+        pageInit: pageInit,
+        filterAll: filterAll,
+        // filterCustomers : filterCustomers,
+        // filterStatus: filterStatus,
+        noSelections : noSelections,
+        clearCustomers : clearCustomers,
+        findBaseUrl : findBaseUrl
+        };
+    
+    });
---
+++ FileCabinet/SuiteScripts/CE_InvoiceSOs_Suitelet.js
@@ -0,0 +1,121 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+
+// Purpose of Suitelet is to invoice any selected sales orders (which can be filtered by customers (multiselect) and individual sales order (checkbox))
+
+define(['N/file', 'N/render', 'N/search', 'N/record', 'N/ui/serverWidget', 'N/task', 'N/url', 'N/runtime', '/SuiteScripts/CE_Invoice_SalesOrders_Suitelet/CE_InvoiceSOs_Common'],
+ /**
+ * @param {file} file
+ * @param {render} render
+ * @param {search} search
+ * @param {record} record
+ * @param {serverWidget} serverWidget
+ * @param {task} task
+ * @param {url} url
+ * @param {runtime} runtime
+ * @param {Object} context
+ * @param {ServerRequest} context.request - Incoming request
+ * @param {ServerResponse} context.response - Suitelet response
+ */
+
+    (file, render, search, record, serverWidget, task, url, runtime, common) => {
+
+        const onRequest = (context) => {            
+            const request = context.request;
+            const response = context.response;
+            
+            if (request.method == 'GET'){
+
+                let mrTask = request.parameters.mr_task_id;
+                let letterCheck = request.parameters.customers_filtered;
+                let status = request.parameters.status;
+                log.debug('status', status);
+                log.debug('status Type', typeof status);
+
+
+                if (letterCheck == 'AAA') {
+                    common.errorNoCustomersToFilter(response);
+                }
+                else if (mrTask) { //checks if map/reduce has started running
+
+                    let statusOfMr = task.checkStatus({
+                        taskId: mrTask
+                    });
+
+                    // writes final invoice page
+                    if (statusOfMr.status === 'COMPLETE') {   
+                        const finalInvoiceForm = common.initializeFinalForm();
+                        common.populateFinalSublist(finalInvoiceForm);                        
+                        common.mrProcessingComplete(response);
+                    }
+                    else {  //writes invoice still processing page
+                        common.mrStillProcessing(statusOfMr, response)
+                    }
+                }
+                else{  
+
+                    //Stores landing page url of suitelet, on custom record,for use by client script
+                    let acctId = runtime.accountId;
+                    let currScript = runtime.getCurrentScript();
+                    let scriptUrl = url.resolveScript({
+                        scriptId: currScript.id,
+                        deploymentId: currScript.deploymentId,
+                        returnExternalURL: true
+                    });
+                    let acctNum = acctId.split('_')[0]
+                    let acctUrl = url.resolveDomain({
+                        hostType: url.HostType.APPLICATION,
+                        accountId: acctNum
+                    });
+                    common.storeSuiteletUrl(acctId, scriptUrl, acctUrl);
+                    
+                    //Writes landing page
+                    
+                    //initializes landing page form
+                    const invoiceForm = common.initializeForm(context, request);
+
+                    //gathers data for landing page form
+                    const salesOrderData = common.getData(request);
+
+                    // populates landing page form
+                    common.populateLandingForm(invoiceForm, request, salesOrderData);
+
+                    //writes landing page form
+                    response.writePage({
+                        pageObject: invoiceForm
+                    });
+                }
+            }
+
+            else {  //POST Request
+
+                //Processes data from landing page form
+                let selectedSalesOrders = request.parameters.custpage_sales_ordersdata;
+                let soData = common.salesOrderSelectionData(selectedSalesOrders);  
+                
+                // If no Sales Orders have been checked before the Invoicing button
+                //      is clicked, renders page with error message and button to return
+                //      to landing page.
+                if (soData.soOrderNums === '') {
+                    common.noSalesOrdersSelected(request, response);
+                }
+
+                else {
+                
+                //Runs map/reduce
+                common.runMapReduce(soData.soIdAndNumber);
+                }
+    
+            }
+        }
+
+        
+
+
+
+        return {onRequest}
+    }
+);
+
